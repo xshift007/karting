eeb57aa5e6567ab8ccd53aadc9d6d14200ba799c
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2025-04-26 13:01:04 -0400

    sprin 3.4.1

--- kartingrm/src/main/java/com/kartingrm/repository/ReservationRepository.java
+++ kartingrm/src/main/java/com/kartingrm/repository/ReservationRepository.java
@@ -1,10 +1,17 @@
 package com.kartingrm.repository;
 
 import com.kartingrm.entity.Reservation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"session", "client"})
+    List<Reservation> findAll();
+
     @Query("""
        SELECT COALESCE(SUM(r.participants),0)
        FROM Reservation r
