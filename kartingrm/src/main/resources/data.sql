2125c67e1d8cbf4907bf6d789f306059435194ea
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2025-04-28 15:38:37 -0400

    docker funcional
    no funciona de manera rapida pero sirve

--- kartingrm/src/main/resources/data.sql
+++ kartingrm/src/main/resources/data.sql
@@ -1,7 +1,4 @@
 
-DESCRIBE tariff_config;
-
-
 INSERT INTO tariff_config (rate, price, minutes) VALUES
                                                      ('LAP_10', 15000, 30),
                                                      ('LAP_15', 20000, 35),
@@ -9,8 +6,6 @@ INSERT INTO tariff_config (rate, price, minutes) VALUES
                                                      ('WEEKEND', 20000, 35),
                                                      ('HOLIDAY', 25000, 40);
 
-                                                     
-DESCRIBE holidays;
 
 INSERT INTO holidays (date, name) VALUES
                                       ('2025-09-18', 'Fiestas Patrias'),
